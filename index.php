26eb300cd9c8be053685f51dbbed37909b5a50d9
Nick <nickwhetstone@siu.edu>
2016-06-01 23:00:23 -0500

    Most of the site's old functionality is now present using Angular
    To-do: Better site design

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="nickwhetstoneApp">
 <head lang="en-US">
 <title>Nick Whetstone | Engineer</title>
 <meta charset="UTF-8">
@@ -7,62 +7,22 @@
 <meta name=viewport content='width=1200'>
 <link rel="stylesheet" href="css/main.css?<?php echo date('l\%20\%20jS\%20of%20F\20Y\%20h:i:s\%20A'); ?>">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-sanitize.js"></script>
 </head>
-<body>
+<body ng-controller="ContentController">
 <div id="wrapper">
 	<div id="links">
 		<div id="left"><a href="https://www.linkedin.com/in/nickwhetstone"><img src="images/LinkedIn.png" alt="LinkedIn" title="My LinkedIn Profile"></a></div>
 			<div id="right"><a href="https://github.com/nickwhetstone"><img src="images/GitHub.png" alt="GitHub" title="My GitHub Profile"></a></div>
 			<div id="center"><img src="images/nick.png" id="picture" alt="" title="Personal picture"><h1>Nick Whetstone</h1><a href="mailto:nickwhetstone@siu.edu">nickwhetstone@siu.edu</a></div>
 	</div>
-	<div id="about">
-		<h2>About me</h2>
-		<div class="content">I'm a twenty-year-old junior undergraduate student pursuing Bachelor of Science degrees in both Electrical Engineering and Computer Engineering at <a href="http://siu.edu">Southern Illinois University Carbondale (SIUC)</a>, an <a href="http://abet.org">ABET-accredited</a> research university. In May 2014, I completed an Associate of Science Degree at <a href="http://www.iecc.edu/page.php?page=WVCH">Wabash Valley College</a> before transferring to SIUC for Fall Semester 2014.</div>
-		<h2>What I'm doing</h2>
-		<div class="content">
-		<ul id="current">
-			<li>
-			<h3>Grade Calculator</h3>
-				<img src="images/GradeCalc.png" title="Android Grade Calculator" alt="Android Grade Calculator" id="gradecalc">
-				I'm fine-tuning an Android app I developed to calculate what students need on final exams to achieve the grade they desire.
-			</li>
-			<li>
-			<h3>SIUC <a href="http://students.sae.org/cds/formulaseries/">Formula SAE</a> Team</h3>
-				I'm working with a team of four others to develop a digital controls
-				system for our competition vehicle using an
-				<a href="https://www.arduino.cc/">Arduino</a>.
-			</li>
-			<li><h3>Courses</h3>
-			I am currently taking courses in the following subjects:
-			<ul>
-				<li>Electromechanical Energy Conversion <a href="http://nwhetstone.com/classes/syllabi/ECE385.pdf">(Syllabus)</a></li>
-				<li>Electronics <a href="http://nwhetstone.com/classes/syllabi/ECE345.doc">(Syllabus)</a></li>
-				<li>Signals and Systems <a href="http://nwhetstone.com/classes/syllabi/ECE355.pdf">(Syllabus)</a></li>
-				<li>Discrete Logic <a href="http://nwhetstone.com/classes/syllabi/ECE225.pdf">(Syllabus)</a></li>
-			</ul>
-			</li>
-			<li><h3><a href="https://github.com/nickwhetstone/baseConvert">baseConvert</a></h3>I'm currently developing a user-friendly command-line tool to convert numeric bases. You can find baseConvert's repository <a href="https://github.com/nickwhetstone/baseConvert">on GitHub</a>.</li>
-		</ul>
-		</div>
-		<h2>What I've done</h2>
-		<div class="content">
-		<ul id="past">
-			<li><a href="http://simplemachines.org/"><img src="images/smf.png" title="Simple Machines Forum" alt="Simple Machines Forum"></a>
-			 For most of 2010, I was an official team member for <a href="http://www.simplemachines.org/about/smf/">Simple Machines Forum (SMF)</a>, the second-largest open source forum software on the internet. The software is written in PHP and uses a MySQL database. As a team member, I contributed to code modifications, technical support, software testing, and bug fixing. I'm <a href="http://www.simplemachines.org/about/smf/team.php#friends">currently credited on SMF's site</a> as a friend of the project.
-			</li>
-			<li>
-			<h3>Courses</h3>Here are a few of the subjects I've taken courses in in the past:
-			<ul>
-				<li>Procedural Programming and Data Structures <a href="http://nwhetstone.com/classes/syllabi/ECE222.pdf">(Syllabus)</a></li>
-				<li>Electric Circuits <a href="http://nwhetstone.com/classes/syllabi/ECE235.pdf">(Syllabus)</a></li>
-				<li>Calculus I, Calculus II, Calculus III, and Introduction to Differential Equations</li>
-			</ul>
-			</li>
-		</ul>
-		</div>
+	<div ng-repeat="block in blocks" compile-html="block">
+		<h2>{{block.name}}</h2>
+		<div class="content" ng-bind-html="block.contents" ng-show="block.open"></div>
 		</div>
 </div>
-<div id="nojs">It seems that your browser has JavaScript disabled, in which case you're really missing out on the best possible experience. Consider turning it back on!</div>
+<div id="nojs" ng-hide="1">It seems that your browser has JavaScript disabled, in which case you're really missing out on the best possible experience. Consider turning it back on!</div>
 <script src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-$(document).ready(function() {
-$("#nojs").hide();
-$("body").hide().fadeIn(600);
-$(".content").hide();
-$("h2").wrapInner("<a href=\"javascript:;\"></a>");
-$("h2 a").prepend("<span class=\"toggle\">+</span>");
+var nickwhetstoneApp = angular.module('nickwhetstoneApp', ['ngSanitize']);
+nickwhetstoneApp.controller('ContentController', function ContentController($scope) {
+	$scope.blocks = [
+		{
+			open: true,
+			name: 'About Me',
+			contents: 'I\'m a twenty-year-old junior undergraduate student pursuing Bachelor of Science degrees in both Electrical Engineering and Computer Engineering at <a href="http://siu.edu">Southern Illinois University Carbondale (SIUC)</a>, an <a href="http://abet.org">ABET-accredited</a> research university. In May 2014, I completed an Associate of Science Degree at <a href="http://www.iecc.edu/page.php?page=WVCH">Wabash Valley College</a> before transferring to SIUC for Fall Semester 2014.'
+		}, {
+			open: false,
+			name: 'What I\'m doing',
+			contents: '<ul id="current"><li> <h3>Grade Calculator</h3> <img src="images/GradeCalc.png" title="Android Grade Calculator" alt="Android Grade Calculator" id="gradecalc"> I\'m fine-tuning an Android app I developed to calculate what students need on final exams to achieve the grade they desire. </li> <li> <h3>SIUC <a href="http://students.sae.org/cds/formulaseries/">Formula SAE</a> Team</h3> I\'m working with a team of four others to develop a digital controls system for our competition vehicle using an <a href="https://www.arduino.cc/">Arduino</a>. </li> <li><h3>Courses</h3> I am currently taking courses in the following subjects: <ul> <li>Electromechanical Energy Conversion <a href="http://nwhetstone.com/classes/syllabi/ECE385.pdf">(Syllabus)</a></li> <li>Electronics <a href="http://nwhetstone.com/classes/syllabi/ECE345.doc">(Syllabus)</a></li> <li>Signals and Systems <a href="http://nwhetstone.com/classes/syllabi/ECE355.pdf">(Syllabus)</a></li> <li>Discrete Logic <a href="http://nwhetstone.com/classes/syllabi/ECE225.pdf">(Syllabus)</a></li> </ul> </li> <li><h3><a href="https://github.com/nickwhetstone/baseConvert">baseConvert</a></h3>I\'m currently developing a user-friendly command-line tool to convert numeric bases. You can find baseConvert\'s repository <a href="https://github.com/nickwhetstone/baseConvert">on GitHub</a>.</li> </ul>'}, {
+			open: false,
+			name: 'What I\'ve done',
+			contents: '<ul id="past"> <li><a href="http://simplemachines.org/"><img src="images/smf.png" title="Simple Machines Forum" alt="Simple Machines Forum"></a> For most of 2010, I was an official team member for <a href="http://www.simplemachines.org/about/smf/">Simple Machines Forum (SMF)</a>, the second-largest open source forum software on the internet. The software is written in PHP and uses a MySQL database. As a team member, I contributed to code modifications, technical support, software testing, and bug fixing. I\'m <a href="http://www.simplemachines.org/about/smf/team.php#friends">currently credited on SMF\'s site</a> as a friend of the project. </li> <li> <h3>Courses</h3>Here are a few of the subjects I\'ve taken courses in in the past: <ul> <li>Procedural Programming and Data Structures <a href="http://nwhetstone.com/classes/syllabi/ECE222.pdf">(Syllabus)</a></li> <li>Electric Circuits <a href="http://nwhetstone.com/classes/syllabi/ECE235.pdf">(Syllabus)</a></li> <li>Calculus I, Calculus II, Calculus III, and Introduction to Differential Equations</li> </ul> </li> </ul>'
+		}
+	];
 });
-
-$("h2").on("click", function() {
-$(this).next("div").slideToggle("fast");
-$(this).find("span").text($(this).find("span").text() === "+" ? "-" : "+");
-});
+$("h2").wrapInner("<a ng-click=\"block.open = !block.open\"></a>");
